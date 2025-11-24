fcc820414312816182772cb56114e22b4c2744c7
Arpita Paul <72724463+paularpi66@users.noreply.github.com>
2025-11-24 20:54:17 +0600

    Update test_contact_form.py

--- tests/test_contact_form.py
+++ tests/test_contact_form.py
@@ -11,6 +11,6 @@ def test_contact_form_validation_and_submission(driver):
     print("--> empty form submited")
     assert contact.has_validation_errors(), 'Expected validation errors when submitting blank form'
 
-    contact.fill_form('Arpita Test', 'arpita@example.com', 'Hello! This is a test message.')
+    contact.fill_form('Arpita', 'Test', 'arpita@example.com', 'Hello! This is a test message. lorem ipsam lorem ipsam lorem ipsam lorem ipsam')
     time.sleep(1)
     assert contact.success_message_present(), 'Expected success message after valid form submission'
